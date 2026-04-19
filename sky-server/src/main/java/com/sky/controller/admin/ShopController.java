2152073a3566e36ef216a2b9a31c1f854b23f9e4
ykk <ykrial@163.com>
2026-04-19 09:48:57 +0800

    简单学习了redis，并新增了设置营业状态的代码

---
+++ sky-server/src/main/java/com/sky/controller/admin/ShopController.java
@@ -0,0 +1,33 @@
+package com.sky.controller.admin;
+
+
+import com.sky.result.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.*;
+
+@RestController("adminShopController")
+@RequestMapping("/admin/shop")
+@Slf4j
+public class ShopController {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @PutMapping("/{status}")
+    public Result setStatus(@PathVariable Integer status){
+
+        log.info("设置营业状态为：{}",status==1 ?"营业中":"打样中");
+        redisTemplate.opsForValue().set("SHOP_STATUS",status);
+        return  Result.success();
+    }
+
+    @GetMapping("/status")
+    public Result<Integer> getStatus(){
+
+        Integer shopStatus = (Integer)redisTemplate.opsForValue().get("SHOP_STATUS");
+        log.info("获取到的营业状态为{}",shopStatus ==1 ?"营业中":"打样中");
+        return Result.success(shopStatus);
+    }
+}
---
+++ sky-server/src/main/java/com/sky/controller/user/ShopController.java
@@ -0,0 +1,27 @@
+package com.sky.controller.user;
+
+
+import com.sky.result.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("userShopController")
+@RequestMapping("/user/shop")
+@Slf4j
+public class ShopController {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @GetMapping("/status")
+    public Result<Integer> getStatus(){
+
+        Integer shopStatus = (Integer)redisTemplate.opsForValue().get("SHOP_STATUS");
+        log.info("获取到的营业状态为{}",shopStatus ==1 ?"营业中":"打样中");
+        return Result.success(shopStatus);
+    }
+}
